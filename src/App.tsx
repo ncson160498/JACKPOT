a166eb507f9fd96bd37db7e646cd31d4f145439e
Nguyễn Công Sơn <91456984+ncson160498@users.noreply.github.com>
2023-06-25 16:14:01 +0700

    ui

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className='bg-black min-h-screen flex flex-col'>
+        <Header/>
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import NavButton from './NavButton';
+import { Bars3BottomRightIcon } from '@heroicons/react/24/solid'
+function Header() {
+  return (
+    <header className='grid grid-cols-2 md:grid-cols-5 justify-between'>
+      <div className='flex items-center space-x-2'>
+        <div className='flex items-center space-x-2'>
+          <img className="rounded-full h-20 w-20" src="jackpot.png" alt="" />
+        </div>
+        <div>
+          <p className='text-xs text-emerald-500 truncate'>User...</p>
+
+        </div>
+      </div>
+      <div className=' hidden md:flex md:col-span-3 items-center justify-center rounded-md'>
+        <div className='bg-[#0A1F1C] p-4 space-x-2'>
+            <NavButton isActive title='Buy Tickets'/>
+            <NavButton title='Logout'/>
+        </div>
+      </div>
+      <div className='flex flex-col ml-auto text-right'>
+        <Bars3BottomRightIcon className='h-8 w-8 mx-auto text-white cursor-pointer'/>
+        <span className='md:hidden'>
+           <NavButton title='Logout'/>
+      </span>      
+      </div>
+     
+    </header>
+  );
+}
+
+export default Header
